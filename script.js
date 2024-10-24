a8a53488f0fdecb4ec6d2e240eddfabe2e4965db
mokarram280 <mdmokarramhossain98@gmail.com>
2024-10-24 11:32:44 +0600

    javascript 2nd part added

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ let taskCount = 0;
 const displayCount = (taskCount)=>{
     countValue.innerText = taskCount;
 };
+
+const addTask = ()=>{
+    const taskName = inputText.value.trim();
+    error.style.display = "none";
+    if(!taskName){
+        setTimeout(()=>{
+            error.style.display = "block";
+        },200);
+        return;
+    }
+
+};

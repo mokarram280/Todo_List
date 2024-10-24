862b3ffc5d4c30ff6a6268f2cf68fc5297bfef76
mokarram280 <mdmokarramhossain98@gmail.com>
2024-10-24 11:17:59 +0600

    javascript 1st part added

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputText = document.querySelector("#inputText");
+const btn = document.querySelector("#btn");
+const error = document.querySelector("#error")
+const taskContainer = document.querySelector("#tasks");
+const countValue = document.querySelector("#countValue");
+
+let taskCount = 0;
+const displayCount = (taskCount)=>{
+    countValue.innerText = taskCount;
+};
